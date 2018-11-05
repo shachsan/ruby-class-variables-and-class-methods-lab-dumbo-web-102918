de353ad1d6e81902344d966cf056933f67917fd0
SANJAY K SAI <shachsan@hotmail.com>
2018-11-05 01:50:25 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,43 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count +=1
+    @@genres << @genre
+    @@artists << @artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genre_count
+    genres_count = Hash.new(0)
+    @@genres.each do |genre|
+      genres_count[genre]+=1
+    end
+    genres_count
+  end
+
+  def self.artist_count
+    artists_count = Hash.new(0)
+    @@artists.each do |artist|
+      artists_count[artist]+=1
+    end
+    artists_count
+  end
+end
